5bd3852025a2f885ad3063de64ea5f8a8006ae21
Marius Pistoresi <marius.pistoresi@gmail.com>
2025-10-13 14:21:29 +0200

    feat: add proxy

---
+++ LetsGoBiking/Proxy/Cache.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Runtime.Caching;
+using System.Threading.Tasks;
+using System.Security.Policy;
+
+namespace Proxy
+{
+
+    public class Cache 
+    {
+        private static readonly CacheItemPolicy DEFAULT_POLICY = new CacheItemPolicy
+        {
+            AbsoluteExpiration = DateTime.Now.AddMinutes(10),
+        };
+
+        private static readonly CacheItemPolicy CONTRACTS_POLICY = new CacheItemPolicy
+        {
+            AbsoluteExpiration = DateTimeOffset.Now.AddDays(2),
+        };
+
+        private static readonly CacheItemPolicy STATIONS_POLICY = DEFAULT_POLICY;
+        private readonly MemoryCache cache = MemoryCache.Default;
+
+        public APIResponse GetRouteCache(string url)
+        {
+            APIResponse response = null;
+
+            if (cache.Contains(url))
+            {
+               response = (APIResponse) cache.Get(url);
+            }
+
+            Console.WriteLine(url + " is cached: " + (response != null));
+
+            return response;
+        }
+
+        public void SetRouteCache(string url, APIResponse response)
+        {
+            Uri uri = new Uri(url);
+
+            string route = uri.Segments.Last().Trim('/');
+
+            CacheItemPolicy policy;
+
+            switch (route)
+            {
+                case "contracts":
+                    policy = CONTRACTS_POLICY;
+                    break;
+
+                case "stations":
+                    policy = STATIONS_POLICY;
+                    break;
+
+                default:
+                    policy = DEFAULT_POLICY;
+                    break;
+            }
+
+            cache.Set(url, response, policy);
+        }
+    }
+}
---
+++ LetsGoBiking/Proxy/ProxyService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Proxy
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class ProxyService : IProxyService
+
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        private Cache cache = new Cache();
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public APIResponse Call(string url)
+        {
+            APIResponse apiResponse = cache.GetRouteCache(url);
+
+            if (apiResponse == null)
+            {
+                apiResponse = new APIResponse();
+                try
+                {
+                    HttpResponseMessage response = client.GetAsync(url).GetAwaiter().GetResult();
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                    apiResponse.Status = (int)response.StatusCode;
+                    apiResponse.Reponse = responseBody;
+
+                    cache.SetRouteCache(url, apiResponse);
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine("Message :{0} ", e.Message);
+
+                    apiResponse.Status = 500;
+                }
+            }
+
+            return apiResponse;
+        }
+    }
+}
